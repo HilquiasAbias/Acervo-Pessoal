88c91cc8c2aacb414b8e47e592974643a5377007
Hilquias Abias Figueiredo Silva <hilquias.a@academico.ifrn.edu.br>
2023-01-16 11:30:19 -0300

    reformulando models de app

--- project/app/models.py
+++ project/app/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 class Contact(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField(max_length=250)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.name
@@ -15,6 +23,7 @@ class Book(models.Model):
     year = models.CharField(max_length=10)
     cover_photo = models.ImageField(null=True)
     created_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.title
@@ -22,14 +31,16 @@ class Book(models.Model):
 class LendingBook(models.Model):
     started = models.DateTimeField(auto_now_add=True)
     returned = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
-    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    contact = models.OneToOneField(Contact, on_delete=models.CASCADE)
+    book = models.OneToOneField(Book, on_delete=models.CASCADE)
 
 class Item(models.Model):
     name = models.CharField(max_length=150)
     description = models.CharField(max_length=200)
     cover_photo = models.ImageField(null=True)
     created_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.name
@@ -37,14 +48,6 @@ class Item(models.Model):
 class LendingItem(models.Model):
     started = models.DateTimeField(auto_now_add=True)
     returned = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
-    item = models.ForeignKey('Item', on_delete=models.CASCADE)
-
-class User(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.EmailField(max_length=250)
-    password = models.CharField(max_length=150)
-    created_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    contact = models.OneToOneField(Contact, on_delete=models.CASCADE)
+    item = models.OneToOneField(Item, on_delete=models.CASCADE)
