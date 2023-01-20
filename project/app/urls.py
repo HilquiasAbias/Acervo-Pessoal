9bc17cd122b151808a52945a08763296745a7458
unknown <hilquias.dev@gmail.com>
2023-01-19 22:46:57 -0300

    iniciando desenvolvimento dos templates

--- project/app/urls.py
+++ project/app/urls.py
@@ -3,6 +3,9 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.ItensView.as_view(), name='index'),
-    path('page_one/', views.page_one, name='page_one')
+    path('', views.IndexView.as_view(), name='index'),
+    path('cadastro/', views.RegisterView.as_view(), name='register'),
+    path('entrar/', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
+    path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
+    path('itens/', views.ItensView.as_view(), name='itens'),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,28 +1,45 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin  # para classes. EX: class MyClass(LoginRequiredMixin, TemplateView)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
 from django.views.generic import TemplateView
-from . import models #Profile, Contact, Item, Book, LendingBook, LendingItem
+from .models import Profile, Contact, Item, Book, LendingBook, LendingItem
 
 class IndexView(TemplateView):
     def get(self, request, *args, **kwargs):
         return render(request, 'app/index.html')
 
+class DashboardView(LoginRequiredMixin, TemplateView):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        return render(request, 'app/dashboard.html', {'user': user})
 
 class ItensView(LoginRequiredMixin, TemplateView):
     def get(self, request, *args, **kwargs):
         user = request.user
-        profile = Profile.objects.filter(user=user)
-        itens = Item.objects.filter(profile=profile)
-        return render(request, 'app/itens.html', {'user': user, 'profile': profile, 'itens': itens})
+        profile = Profile.objects.filter(user=user).first()
+        #itens = Item.objects.get(profile=profile)
+        return render(request, 'app/itens.html', {'user': user, 'profile': profile}) #, 'itens': itens
 
     def post(self, request, *args, **kwargs):
+        return HttpResponseRedirect(reverse('acervo:index'))
 
+class RegisterView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'app/register.html')
 
-        return HttpResponseRedirect(reverse('app:index'))
+    def post(self, request, *args, **kwargs):
+        username = request.POST['user']
+        email = request.POST['email']
+        password = request.POST['password']
 
-@login_required
-def page_one(request):
-    return render(request, 'app/page_one.html', { 'name': request.user.username })
+        userExists = User.objects.filter(email=email).first()
+        if userExists:
+            return HttpResponse('Já existe com esse email.')
 
+        user = User.objects.create_user(username=username, email=email, password=password)
+        print(user)
+        user.save()
+        return HttpResponseRedirect('app/login.html')
